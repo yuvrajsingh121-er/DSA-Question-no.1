cee940e284ffaefa8ef2214591c658f71fd844dd
Yuvraj Singh <ys792608@gmail.com>
2026-04-04 02:08:37 +0530

    Add files via upload

---
+++ BasicJava.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class BasicJava {
+    public static void main(String[] args)
+     {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+        sc.close();
+
+    }
+}
+
