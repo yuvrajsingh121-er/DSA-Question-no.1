9f783f7b1827692f8539a6836bcf3799cdb73b2c
Yuvraj Singh <ys792608@gmail.com>
2026-04-04 02:13:38 +0530

    Update BasicJava.java

--- BasicJava.java
+++ BasicJava.java
@@ -4,10 +4,12 @@ public class BasicJava {
     public static void main(String[] args)
      {
         Scanner sc = new Scanner(System.in);
+         System.out.println("enter value of a");
         int a = sc.nextInt();
+         System.out.println("enter value of b");
         int b = sc.nextInt();
         int sum = a + b;
-        System.out.println(sum);
+        System.out.println("Sum of two  number: "sum);
         sc.close();
 
     }
